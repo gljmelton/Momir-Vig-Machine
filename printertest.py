bce4fa6b62d14602b414887fd97fa93dba8fc3de
gljmelton <gljmelton@gmail.com>
2026-04-08 19:33:29 -0700

    fix printer init

--- printertest.py
+++ printertest.py
@@ -1,7 +1,7 @@
 #https://thermalprinter.readthedocs.io/usage.html#instantiate-the-class
 from thermalprinter import ThermalPrinter
 
-printer = ThermalPrinter("/dev/serial0", 9600)
+printer = ThermalPrinter(port="/dev/serial0", baudrate=9600)
 
 printer.out("Show time, here comes the demonstration!")
 printer.feed()
