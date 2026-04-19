24c9dac448d2fa3337912a4ba63482a4cfef31fe
gljmelton <gljmelton@gmail.com>
2026-04-19 14:06:13 -0700

    add printer helper for printing test cards from commandline

--- printertest.py
+++ printertest.py
@@ -1,8 +1,30 @@
 #https://thermalprinter.readthedocs.io/usage.html#instantiate-the-class
 from thermalprinter import ThermalPrinter
+import scryfall
+import argparse
+import printerhelper
 
-printer = ThermalPrinter(port="/dev/serial0", baudrate=9600)
+imagecardid="0ab91962-ebad-46f6-9f90-7477c224d93d"
+doublefacecardid="b3819a11-2f3e-4304-a1b0-6abf893c89c5"
+testcardid="6a0b230b-d391-4998-a3f7-7b158a0ec2cd"
 
-printer.out("Show time, here comes the demonstration!")
-printer.feed()
-printer.demo()
+parser = argparse.ArgumentParser("printer_test")
+parser.add_argument("--image", help="Prints a test image")
+parser.add_argument("--double_face", help="Prints a double-faced card")
+parser.add_argument("--card", help="Prints a single-faced card")
+args = parser.parse_args()
+
+printer = ThermalPrinter(port="/dev/serial0", baudrate=9600, heat_time=160)
+
+if args.image:
+    imagecard = scryfall.getcardbyid(imagecardid)
+    img = scryfall.getimageforcard(imagecard)
+    printerhelper.customimage(printer, img)
+
+if args.double_face:
+    dfc = scryfall.getcardbyid(doublefacecardid)
+    printerhelper.printcard(printer, dfc)
+
+if args.card:
+    card = scryfall.getcardbyid(testcardid)
+    printerhelper.printcard(printer, card)
